c2647677982a4802d7a98726258acbda9c52f209
DaTikh <baptiste.rogeon@gmail.com>
2018-05-21 19:22:58 +0200

    push heroku

---
+++ app/assets/javascripts/calc.js
@@ -0,0 +1,63 @@
+//---- VARIABLES ----------------------------------------------
+
+let buffer = 0
+let operatingBuffer = 0
+let actionBuffer = ""
+
+//---- FONCTIONS MATHS ----------------------------------------
+
+let add = function() {
+  return operatingBuffer += +buffer;
+}
+
+let substract = function() {
+  return operatingBuffer -= +buffer;
+}
+
+let multiply = function() {
+  return operatingBuffer *= +buffer;
+}
+
+let divide = function() {
+  return operatingBuffer /= +buffer;
+}
+
+//---- RESET BUFFERS ----------------------------------------
+
+let clearBuffer = function() {
+  return buffer = 0;
+}
+
+let clearAction = function() {
+  return actionBuffer = "";
+}
+
+let resetAll =  function() {
+  clearBuffer();
+  clearAction();
+  return operatingBuffer = 0;
+
+}
+
+//---- INPUTS -----------------------------------------------
+
+let inputValue = function(value) {
+  return buffer += value;
+}
+
+let inputAction = function(id) {
+  if (actionBuffer != "") {
+    eval(actionBuffer);
+  } else {
+    operatingBuffer += +buffer;
+  }
+  clearBuffer();
+  return actionBuffer = id;
+}
+
+let inputResult = function() {
+  eval(actionBuffer);
+  clearAction();
+  clearBuffer();
+  return
+}
---
+++ app/assets/javascripts/exos.js
@@ -0,0 +1,41 @@
+let myMax = function(array) {
+  let i = 0;
+  let max = array[0];
+    while (i < array.length) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+      i++;
+    }
+    return max;
+}
+
+//-----------------------------------------------------
+
+const VOWELS = ["a", "e", "i", "o", "u", "y"]
+
+let vowelCount = function(string) {
+  let count = 0;
+    for (char of string) {
+      if (VOWELS.includes(char)) {
+        count++;
+      }
+    }
+  return count;
+}
+
+//-----------------------------------------------------
+
+let reverse = function(string) {
+  let result = string.split("").reverse().join("");
+return result;
+}
+
+//-----------------------------------------------------
+
+let phrase = "Ceci est un test !"
+let tableau = [3, 1, 42, 32, 89]
+// 
+// alert( vowelCount(phrase) );
+// alert( myMax(tableau) );
+// alert( reverse(phrase) );
